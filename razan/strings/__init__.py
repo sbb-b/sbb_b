b3380e311f3bbdffde9a3163bd9a27ffb952046d
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-05-26 06:43:55 +0300

    Add files via upload

--- razan/strings/__init__.py
+++ razan/strings/__init__.py
@@ -1,1 +1,5 @@
-
+from ._fun import *
+from .hack import *
+from .helper import *
+from .JASEM1 import JASEM1
+from .TR import TTRA1
---
+++ razan/strings/helper.py
@@ -0,0 +1,55 @@
+import os
+import re
+
+import requests
+from telethon import Button
+
+try:
+    from pyquery import PyQuery as pq
+except ModuleNotFoundError:
+    os.system("pip3 install pyquery")
+    from pyquery import PyQuery as pq
+
+
+def get_download_url(link):
+    post_request = requests.post(
+        "https://www.expertsphp.com/download.php", data={"url": link}
+    )
+
+    request_content = post_request.content
+    str_request_content = str(request_content, "utf-8")
+    download_url = pq(str_request_content)("table.table-condensed")("tbody")("td")(
+        "a"
+    ).attr("href")
+    return download_url
+
+
+def get_msg_button(texts: str):
+    btn = []
+    for z in re.findall("\\[(.*?)\\|(.*?)\\]", texts):
+        text, url = z
+        urls = url.split("|")
+        url = urls[0]
+        # if not is_url_ok(url):
+        #    continue
+        if len(urls) > 1:
+            btn[-1].append([text, url])
+        else:
+            btn.append([[text, url]])
+
+    txt = texts
+    for z in re.findall("\\[.+?\\|.+?\\]", texts):
+        txt = txt.replace(z, "")
+
+    return txt.strip(), btn
+
+
+def create_tl_btn(button: list):
+    btn = []
+    for z in button:
+        if len(z) > 1:
+            kk = [Button.url(x, y.strip()) for x, y in z]
+            btn.append(kk)
+        else:
+            btn.append([Button.url(z[0][0], z[0][1].strip())])
+    return btn
