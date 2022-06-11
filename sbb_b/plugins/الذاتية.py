a395034b1e8f6dab9412f516a412ef6fc1524c63
sbb-b <106477733+sbb-b@users.noreply.github.com>
2022-06-12 00:25:43 +0300

    Update الذاتية.py

--- sbb_b/plugins/الذاتية.py
+++ sbb_b/plugins/الذاتية.py
@@ -1,29 +1,11 @@
 from sbb_b import sbb_b
-
+from telethon import events
+from os import remove
 
 @sbb_b.ar_cmd(pattern="(جلب الصورة|ذاتية)")
-async def roz(bakar):
-    if not bakar.is_reply:
-        return await bakar.edit(
-            "**❃ يجب عليك الرد على صورة ذاتيه التدمير او صورة مؤقته**"
-        )
-    rr9r7 = await bakar.get_reply_message()
-    pic = await rr9r7.download_media()
-    await sbb_b.send_file(
-        "me", pic, caption=f"**⪼ عزيزي هذه هي الصورة او الفيديو التي تم حفظه هنا**"
-    )
-    await bakar.delete()
-
-
-@sbb_b.ar_cmd(pattern="^هممم$")
-async def roz(bakar):
-    if not bakar.is_reply:
-        return await bakar.edit(
-            "**❃ يجب عليك الرد على صورة ذاتيه التدمير او صورة مؤقته**"
-        )
-    rr9r7 = await bakar.get_reply_message()
-    pic = await rr9r7.download_media()
-    await sbb_b.send_file(
-        "me", pic, caption=f"**⪼ عزيزي هذه هي الصورة او الفيديو التي تم حفظه هنا**"
-    )
-    await bakar.edit("هممم")
+async def datea(event):
+    await event.delete()
+    scertpic = await event.get_reply_message()
+    downloadjmthon = await scertpic.download_media()
+    send = await sbb_b.send_file("me", downloadjmthon)
+    remove(downloadjmthon)
