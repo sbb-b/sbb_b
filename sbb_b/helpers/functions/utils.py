dc7f49ff078cfc4107bf3c764f42f406d441b582
sbb-b <106477733+sbb-b@users.noreply.github.com>
2022-07-16 23:22:10 +0300

    Update utils.py

--- sbb_b/helpers/functions/utils.py
+++ sbb_b/helpers/functions/utils.py
@@ -1,9 +1,24 @@
 import time
 from datetime import datetime
 
-#from emoji import get_emoji_regexp
+import re
 from telethon.tl.types import Channel, PollAnswer
 
+JMTHON_PATTERN = re.compile(
+    "["
+    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+    "\U0001F700-\U0001F77F"  # alchemical symbols
+    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+    "\U0001FA00-\U0001FA6F"  # Chess Symbols
+    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+    "\U00002702-\U000027B0"  # Dingbats
+    "]+"
+)
 
 async def get_message_link(channelid, msgid):
     if str(channelid).startswith("-"):
@@ -94,4 +109,8 @@ async def extract_time(sbb_b, time_val):
 
 def Build_Poll(options):
     return [PollAnswer(option, bytes(i)) for i, option in enumerate(options, start=1)]
+
+def deEmojify(inputString: str) -> str:
+    """Remove emojis and other non-safe characters from string"""
+    return re.sub(JMTHON_PATTERN, "", inputString)
 
