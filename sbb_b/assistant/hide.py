cbcbd15fc96e8ba536b117013cf9c13e78ff9ab4
MUHAMMAD <104639251+R0R77@users.noreply.github.com>
2022-07-21 22:26:32 +0300

    Update hide.py

--- sbb_b/assistant/hide.py
+++ sbb_b/assistant/hide.py
@@ -10,8 +10,8 @@ from sbb_b import sbb_b
 @sbb_b.tgbot.on(CallbackQuery(data=re.compile(b"hide_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
-    if os.path.exists("./userbot/hide.txt"):
-        jsondata = json.load(open("./userbot/hide.txt"))
+    if os.path.exists("./sbb_b/hide.txt"):
+        jsondata = json.load(open("./sbb_b/hide.txt"))
         try:
             reply_pop_up_alert = jsondata[f"{timestamp}"]["text"]
         except KeyError:
