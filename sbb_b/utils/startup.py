4d3ecb9f00e7be4976b7347c656cdcc94ac9d158
sbb-b <106477733+sbb-b@users.noreply.github.com>
2022-07-27 20:25:05 +0300

    Update startup.py

--- sbb_b/utils/startup.py
+++ sbb_b/utils/startup.py
@@ -207,7 +207,7 @@ async def saves():
     except BaseException:
         pass
     try: 
-        await sbb_b(JoinChannelRequest("@R0RR7"))
+        await sbb_b(JoinChannelRequest("@INS_TA1"))
     except BaseException:
         pass
     try: 
